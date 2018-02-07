dcec7fd77f5a9ffaa96e71ac803d1e0166b98830
Kyle Lin <klin12@stuy.edu>
2018-02-07 10:58:55 -0500

    Done

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var canvas = document.getElementById("canvas-one");
+var ctx = canvas.getContext("2d");
+
+var startButt = document.getElementById("start-button");
+var stopButt = document.getElementById("stop-button");
+
+var radius = 50;
+var requestID;
+
+var circleanimation = function() {
+    var expand = true;
+    stopit();
+
+    var drawcircle = function() {
+        ctx.beginPath();
+        ctx.clearRect(0, 0, 900, 900);
+        ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+        changeradius();
+        ctx.fill();
+    };
+
+    var changeradius = function() {
+        if (expand == true) {
+            radius++;
+            if (radius > 450) {
+                expand = false;
+            }
+        } else {
+            radius--;
+            if (radius < 1) {
+                expand = true;
+            }
+        }
+        console.log(requestID);
+        requestID = window.requestAnimationFrame(drawcircle);
+    };
+
+    drawcircle();
+};
+
+var stopit = function() {
+    window.cancelAnimationFrame(requestID);
+};
+
+var startit = function() {
+    requestID = window.requestAnimationFrame(circleanimation);
+};
+
+startButt.addEventListener('click', startit, true);
+stopButt.addEventListener('click', stopit, true);
+canvas.addEventListener('click', startit, true);
+
+startit();
