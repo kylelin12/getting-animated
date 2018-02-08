7b59579375313a1f03fb7d96a139d9456612f6b1
Kyle Lin <klin12@stuy.edu>
2018-02-08 13:23:38 -0500

    Finished DVD Screensaver

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ var ctx = canvas.getContext("2d");
 
 var startButt = document.getElementById("start-button");
 var stopButt = document.getElementById("stop-button");
+var toggleButt = document.getElementById("toggle-type-button");
 
 var radius = 50;
 var requestID;
 
+// Circles
 var expand = true;
 var opacityChange = true;
+
+var typeToggle = 0;
 
 var circleAnimation = function() {
     stopIt();
@@ -18,7 +22,7 @@ var circleAnimation = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
         changeRadius();
-        ctx.fillStyle = "rgba(0, 0, 0, " + genOpacity() +")";
+        ctx.fillStyle = genRGBAColor();
         ctx.fill();
     };
 
@@ -38,23 +42,100 @@ var circleAnimation = function() {
         requestID = window.requestAnimationFrame(drawCircle);
     };
 
-    var genOpacity = function() {
-        return radius / 450;
+    var genRGBAColor = function() {
+        var opacity = radius / 450;
+        var color = 255 - (opacity * 255);
+        var result = "rgba( " + color + ", " + color + ", " + color +", " + opacity + ")"
+        return result;
     };
 
     drawCircle();
+};
+
+var dvdAnimation = function() {
+    stopIt();
+
+    var wDis = 93;
+    var hDis = 48;
+
+    var xVelocity = 13;
+    var yVelocity = 17;
+
+    var xCor = 0;
+    var yCor = 0;
+
+    var drawDVD = function() {
+        for (var i = 0; i < 1; i++) {
+            xCor += xVelocity;
+            yCor += yVelocity;
+
+            if (xCor + wDis >= canvas.width) { // Hit right
+                xVelocity = -xVelocity;
+            }
+            if (yCor + hDis >= canvas.height) { // Hit bottom
+                yVelocity = -yVelocity;
+            }
+            if (xCor == 0) {
+                xVelocity = -xVelocity; // Hit left
+            }
+            if (yCor == 0) {
+                yVelocity = -yVelocity; // Hit top
+            }
+        }
+
+        clearIt();
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "#00AEEF";
+        ctx.fillRect(xCor, yCor, wDis, hDis);
+
+        requestID = requestAnimationFrame(drawDVD);
+    }
+
+    drawDVD();
+};
+
+var clearIt = function() {
+    ctx.clearRect(0, 0 , canvas.width, canvas.height);
+    switch(typeToggle) {
+        case 0: // Circle
+            break;
+        case 1: // DVD Screensaver
+            ctx.fillStyle = "#000000";
+    }
 };
 
 var stopIt = function() {
     window.cancelAnimationFrame(requestID);
 };
 
+var toggleIt = function() {
+    stopIt();
+    switch(typeToggle) {
+        case 0: // Circle
+            typeToggle = 1;
+            break;
+        case 1: // DVD Screensaver
+            typeToggle = 0;
+            break;
+    }
+    clearIt();
+    startIt();
+}
+
 var startIt = function() {
-    requestID = window.requestAnimationFrame(circleAnimation);
+    switch(typeToggle) {
+        case 0: // Circle
+            requestID = window.requestAnimationFrame(circleAnimation);
+            break;
+        case 1: // DVD Screensaver
+            requestID = window.requestAnimationFrame(dvdAnimation);
+            break;
+    }
 };
 
 startButt.addEventListener('click', startIt, true);
 stopButt.addEventListener('click', stopIt, true);
+toggleButt.addEventListener('click', toggleIt, true);
 canvas.addEventListener('click', startIt, true);
 
 startIt();
